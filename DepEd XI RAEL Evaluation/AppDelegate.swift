de939182254a38fc6bc3c29016ab5d0eef6d1945
Kevin Milo <kevinmilo.it@gmail.com>
2018-04-10 05:49:40 +0800

    Initial Drawer Menu

--- DepEd XI RAEL Evaluation/AppDelegate.swift
+++ DepEd XI RAEL Evaluation/AppDelegate.swift
@@ -17,6 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.statusBarStyle = .lightContent
+        UINavigationBar.appearance().barTintColor = UIColor(red: 4/255.0, green: 80/255.0, blue: 140/255.0, alpha: 1.0)
+        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
+    
         return true
     }
 
--- DepEd XI RAEL Evaluation/ViewController.swift
+++ DepEd XI RAEL Evaluation/ViewController.swift
@@ -12,7 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let mainVC = UIViewController()
+        let bgColor = UIColor(red: 232/255.0, green: 232/255.0, blue: 232/255.0, alpha: 1.0)
+        mainVC.view.backgroundColor = bgColor
+        
+        let rootController = RootViewController(mainViewController: mainVC, topNavigationLeftImage: UIImage(named: "hamburger-menu-icon"))
+        let menuVC = MenuViewController()
+        let blueDrawerColor = UIColor(red: 1/255.0, green: 61/255.0, blue: 108/255.0, alpha: 0.95)
+        menuVC.view.backgroundColor = blueDrawerColor
+        
+        let drawerVC = DrawerController(rootViewController: rootController, menuController: menuVC)
+        
+        self.addChildViewController(drawerVC)
+        view.addSubview(drawerVC.view)
+        drawerVC.didMove(toParentViewController: self)
     }
 
     override func didReceiveMemoryWarning() {
